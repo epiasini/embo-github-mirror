151d6cb4967f208aedf5beefd730a8ed4b83b8d1
Eugenio Piasini <eugenio.piasini@gmail.com>
2018-07-11 13:26:57 -0400

    Enable multiple restarts of the minimization procedure

--- embo/embo.py
+++ embo/embo.py
@@ -1,4 +1,5 @@
 from __future__ import division
+
 import numpy as np
 
 from scipy.stats import entropy
@@ -21,13 +22,14 @@ def empirical_bottleneck(x,y,numuniquex=0,numuniquey=0,**kw):
     return i_p,i_f,beta,mi,entropy(px,base=2),entropy(py,base=2)
 
 
-def IB(px,py,pyx_c,maxbeta=5,numbeta=30,iterations=100):
+def IB(px,py,pyx_c,maxbeta=5,numbeta=30,iterations=100,restarts=3):
     """Compute an Information Bottleneck curve
 
     px: marginal probability distribution for the past
     py: marginal distribution for the future
     maxbeta: the maximum value of beta to use to compute the curve
     iterations: number of iterations to use to for the curve to converge for each value of beta
+    restarts: number of times the optimization procedure should be restarted (for each value of beta) from different random initial conditions.
     
     return vectors of ipast and ifuture (ips and ifs respectively) for
     different values of beta (bs)
@@ -38,30 +40,40 @@ def IB(px,py,pyx_c,maxbeta=5,numbeta=30,iterations=100):
     ips = np.zeros(bs.size)
     ifs = np.zeros(bs.size)
     for bi in range(bs.size):
-        # initialize distribution for bottleneck variable
-        pm = np.random.rand(pm_size)+1
-        pm /= pm.sum()
-        pym_c = np.random.rand(py.size,pm.size)+1 # Starting point for the algorithm
-        pym_c /= pym_c.sum(axis=0)
-        # iterate the BA algorithm
-        for i in range(iterations):
-            pmx_c = p_mx_c(pm,px,py,pyx_c,pym_c,bs[bi])
-            pm = p_m(pmx_c,px)
-            pym_c = p_ym_c(pm,px,py,pyx_c,pmx_c)
-            if i>0 and np.allclose(pmx_c,pmx_c_old):
-                # if the x->m mapping is not updating any more, we're at convergence and we can stop
-                break
-            pmx_c_old = pmx_c
-
-        ips[bi] = mi_x1x2_c(pm,px,pmx_c)
-        ifs[bi] = mi_x1x2_c(py,pm,pym_c)
+        candidates = []
+        for r in range(restarts):
+            # initialize distribution for bottleneck variable
+            pm = np.random.rand(pm_size)+1
+            pm /= pm.sum()
+            pym_c = np.random.rand(py.size,pm.size)+1 # Starting point for the algorithm
+            pym_c /= pym_c.sum(axis=0)
+            # iterate the BA algorithm
+            for i in range(iterations):
+                pmx_c, z = p_mx_c(pm,px,py,pyx_c,pym_c,bs[bi])
+                pm = p_m(pmx_c,px)
+                pym_c = p_ym_c(pm,px,py,pyx_c,pmx_c)
+                if i>0 and np.allclose(pmx_c,pmx_c_old):
+                    # if the x->m mapping is not updating any more, we're at convergence and we can stop
+                    break
+                pmx_c_old = pmx_c
+            candidates.append({'past_info' : mi_x1x2_c(pm, px, pmx_c),
+                               'future_info' : mi_x1x2_c(py, pm, pym_c),
+                               'functional' : -np.log2(np.inner(z,px))})
+        # among the restarts, select the result that gives the minimum
+        # value for the functional we're actually minimizing (eq 29 in
+        # Tishby et al 2000).
+        selected_candidate = min(candidates, key=lambda c: c['functional'])
+        ips[bi] = selected_candidate['past_info']
+        ifs[bi] = selected_candidate['future_info']
     # restrict the returned values to those that, at each value of
     # beta, actually increase (for Ipast) and do not decrease (for
     # Ifuture) the information with respect to the previous value of
     # beta. This is to avoid confounds from cases where the AB
     # algorithm gets stuck in a local minimum.
-    ub, betas = compute_upper_bound(ips, ifs, bs)
-    return np.squeeze(ub[:,0]), np.squeeze(ub[:,1]), betas
+    ub, bs = compute_upper_bound(ips, ifs, bs)
+    ips = np.squeeze(ub[:,0])
+    ifs = np.squeeze(ub[:,1])
+    return ips, ifs, bs
 
 
 def p_mx_c(pm,px,py,pyx_c,pym_c,beta):
@@ -78,7 +90,9 @@ def p_mx_c(pm,px,py,pyx_c,pym_c,beta):
     for mi in range(pm.size):
         for xi in range(px.size):
             pmx_c[mi,xi] = pm[mi] * np.exp(-beta * entropy(pyx_c[:,xi], pym_c[:,mi], base=2))
-    return pmx_c/pmx_c.sum(axis=0) #Normalize 
+    z = pmx_c.sum(axis=0)
+    pmx_c /= z #Normalize 
+    return pmx_c, z
 
 
 def p_ym_c(pm,px,py,pyx_c,pmx_c):
