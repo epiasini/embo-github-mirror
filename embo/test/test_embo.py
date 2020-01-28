54112e3aa30016cc6d1dfc7199c8396c11b076d8
Jon Levine <jonalevine1@gmail.com>
2020-01-27 21:13:48 -0500

    refactor embo to be class based

--- embo/test/test_embo.py
+++ embo/test/test_embo.py
@@ -3,27 +3,36 @@ import numpy as np
 
 import embo
 
-def test_origin(x,y):
-    """Check that the IB bound starts at (0,0) for small beta"""
-    i_p,i_f,_ = embo.empirical_bottleneck(x,y)
-    np.testing.assert_allclose((i_p[0],i_f[0]),(0,0),rtol=1e-7,atol=1e-9)
 
-def test_asymptote(x,y):
+def test_origin(x, y):
+    """Check that the IB bound starts at (0,0) for small beta"""
+    eb = embo.EmpiricalBottleneck(x, y)
+    i_p = eb.get_ipast()
+    i_f = eb.get_ifuture()
+    np.testing.assert_allclose((i_p[0], i_f[0]), (0,0), rtol=1e-7, atol=1e-9)
+
+
+def test_asymptote(x, y):
     """Check that the IB bound saturates at (H(x),MI(X:Y)) for large beta.
     
     Note that both H(X) and MI(X,Y) are computed using the functions
     defined within EMBO.
 
     """
-    i_p,i_f,beta,mi,hx,hy = embo.empirical_bottleneck(x,y,maxbeta=10,return_entropies=True)
-    np.testing.assert_allclose((i_p[-1],i_f[-1]),(hx,mi),rtol=1e-7)
+    eb = embo.EmpiricalBottleneck(x, y, maxbeta=10)
+    i_p = eb.get_ipast()
+    i_f = eb.get_ifuture()
+    mi = eb.get_saturation_point()
+    hx, hy = eb.get_entropies()
+
+    np.testing.assert_allclose((i_p[-1], i_f[-1]), (hx, mi), rtol=1e-7)
 
 
 class TestBinarySequence(unittest.TestCase):
     def setUp(self):
         # Fake data sequence
-        self.x = np.array([0,0,0,1,0,1,0,1,0,1])
-        self.y = np.array([1,0,1,0,1,0,1,0,1,0])
+        self.x = np.array([0, 0, 0, 1, 0, 1, 0, 1, 0, 1])
+        self.y = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
 
     def test_origin(self):
         """Check beta->0 limit for binary sequence"""
@@ -33,27 +42,29 @@ class TestBinarySequence(unittest.TestCase):
         """Check beta->infinity limit for binary sequence"""
         test_asymptote(self.x, self.y)
 
+
 class TestUpperBound(unittest.TestCase):
     def setUp(self):
-        self.a = np.array([[0,0],[1,1],[2,0],[3,3],[3,4],[2,5],[4,6],[2,7],[3,8]])
+        self.a = np.array([[0, 0], [1, 1], [2, 0], [3, 3], [3, 4], [2, 5], [4, 6], [2, 7], [3, 8]])
         self.betas = np.arange(self.a.shape[0])
-        self.true_idxs = np.array([0,1,3,6], dtype=np.int)
+        self.true_idxs = np.array([0, 1, 3, 6], dtype=np.int)
 
     def test_upper_bound(self):
         """Check extraction of upper bound in IB space"""
-        u = embo.compute_upper_bound(self.a[:,0], self.a[:,1])
-        np.testing.assert_array_equal(u, self.a[self.true_idxs,:])
+        u = embo.compute_upper_bound(self.a[:, 0], self.a[:, 1])
+        np.testing.assert_array_equal(u, self.a[self.true_idxs, :])
 
     def test_betas(self):
         """Check extraction of beta values related to upper bound in IB space"""
         u, betas = embo.compute_upper_bound(self.a[:,0], self.a[:,1], self.betas)
         np.testing.assert_array_equal(betas, self.betas[self.true_idxs])
-        
+
+
 class TestArbitraryAlphabet(unittest.TestCase):
     def setUp(self):
         # Fake data sequence
-        self.x = np.array([0,0,0,2,0,2,0,2,0,2])
-        self.y = np.array([3.5,0,3.5,0,3.5,0,3.5,0,3.5,0])
+        self.x = np.array([0, 0, 0, 2, 0, 2, 0, 2, 0, 2])
+        self.y = np.array([3.5, 0, 3.5, 0, 3.5, 0, 3.5, 0, 3.5, 0])
 
     def test_origin(self):
         """Check beta->0 limit for sequence with arbitrary alphabet"""
@@ -62,6 +73,7 @@ class TestArbitraryAlphabet(unittest.TestCase):
     def test_asymptote(self):
         """Check beta->infinity limit for sequence with arbitrary alphabet"""
         test_asymptote(self.x, self.y)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
--- embo/utils.py
+++ embo/utils.py
@@ -3,7 +3,8 @@ from __future__ import division
 import numpy as np
 from scipy.stats import entropy
 
-def p_joint(x1,x2,windowx1=1,windowx2=1):
+
+def p_joint(x1, x2, windowx1=1, windowx2=1):
     """
     Compute the joint distribution between two data series
     
@@ -22,14 +23,16 @@ def p_joint(x1,x2,windowx1=1,windowx2=1):
     numwordsx2 = numuniquex2**windowx2
     aux_base_x1 = numuniquex1**np.arange(windowx1)[::-1]
     aux_base_x2 = numuniquex2**np.arange(windowx2)[::-1]
-    px1x2 = np.zeros((numwordsx1,numwordsx2)) #matrix of size numwordsx,numwordsy with for the joint probability distribution
+    # matrix of size numwordsx, numwordsy with for the joint probability distribution
+    px1x2 = np.zeros((numwordsx1,numwordsx2))
     for i in range(len(x1)-(windowx1-1)):
         x1i = np.inner(x1[i:i+windowx1], aux_base_x1).astype(np.int)
         x2i = np.inner(x2[i:i+windowx2], aux_base_x2).astype(np.int)
         px1x2[x1i,x2i] += 1
     return px1x2/px1x2.sum()
 
-def mi_x1x2_c(px1,px2,px1x2_c):
+
+def mi_x1x2_c(px1, px2, px1x2_c):
     """Compute the MI between two probability distributions x1 and x2
     using their respective marginals and conditional distribution
     """
@@ -38,6 +41,7 @@ def mi_x1x2_c(px1,px2,px1x2_c):
     for x2i in range(px2.size):
         conditional_entropy += px2[x2i] * entropy(px1x2_c[:,x2i], base=2)
     return marginal_entropy - conditional_entropy
+
 
 def compute_upper_bound(IX, IY, betas=None):
     """Extract the upper part of the convex hull of an IB sequence.
@@ -72,7 +76,7 @@ def compute_upper_bound(IX, IY, betas=None):
         if points[idx,0]>points[selected_idxs[-1],0] and points[idx,1]>=points[selected_idxs[-1],1]:
             selected_idxs.append(idx)
             
-    upper_bound = points[selected_idxs,:]
+    upper_bound = points[selected_idxs, :]
 
     if betas is None:        
         return upper_bound
