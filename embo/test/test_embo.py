c5cd0ec0fd94b061b736f9802146085caa761017
Eugenio Piasini <eugenio.piasini@gmail.com>
2021-02-06 00:29:48 +0100

    Add more tests: random input sequences, or data with NaNs and Infs

--- embo/test/test_embo.py
+++ embo/test/test_embo.py
@@ -23,28 +23,28 @@ import embo
 
 from scipy.stats import entropy as spentropy
 
-def test_origin(x, y):
+def test_origin(x, y, maxbeta=10):
     """Check that the IB bound starts at (0,0) for small beta"""
-    eb = embo.EmpiricalBottleneck(x, y)
+    eb = embo.EmpiricalBottleneck(x, y, maxbeta=maxbeta)
     i_x = eb.get_ix()
     i_y = eb.get_iy()
     np.testing.assert_allclose((i_x[0], i_y[0]), (0,0), rtol=1e-7, atol=1e-9)
 
 
-def test_asymptote(x, y):
+def test_asymptote(x, y, maxbeta=10):
     """Check that the IB bound saturates at (H(x),MI(X:Y)) for large beta.
     
     Note that both H(X) and MI(X,Y) are computed using the functions
     defined within EMBO.
 
     """
-    eb = embo.EmpiricalBottleneck(x, y, maxbeta=10)
+    eb = embo.EmpiricalBottleneck(x, y, maxbeta=maxbeta)
     i_x = eb.get_ix()
     i_y = eb.get_iy()
     mi = eb.get_saturation_point()
     hx, hy = eb.get_entropies()
 
-    np.testing.assert_allclose((i_x[-1], i_y[-1]), (hx, mi), rtol=1e-7)
+    np.testing.assert_allclose((i_x[-1], i_y[-1]), (hx, mi), rtol=1e-7, atol=1e-9)
 
 
 class TestEntropyFunctions(unittest.TestCase):
@@ -90,17 +90,68 @@ class TestEntropyFunctions(unittest.TestCase):
 
 class TestBinarySequence(unittest.TestCase):
     def setUp(self):
-        # Fake data sequence
+        # Simple data sequence
         self.x = np.array([0, 0, 0, 1, 0, 1, 0, 1, 0, 1])
         self.y = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+        # Random data sequence
+        self.rng = np.random.default_rng()
+        probs = np.array([0.05, 0.45, 0.45, 0.05]) # 0:00 1:01 2:10 3:11
+        cumprobs = np.cumsum(probs)
+        rand = self.rng.random(1000)
+        self.xycorr = np.zeros_like(rand)
+        for cp in cumprobs[:-1]:
+            self.xycorr[rand>=cp] += 1
+        self.xcorr = np.zeros_like(self.xycorr, dtype=np.float)
+        self.ycorr = np.zeros_like(self.xycorr, dtype=np.float)
+        self.xcorr[np.logical_or(self.xycorr==2, self.xycorr==3)] = 1
+        self.ycorr[np.logical_or(self.xycorr==1, self.xycorr==3)] = 1
+
+        self.x_with_nan = np.copy(self.x).astype(np.float)
+        self.x_with_nan[-1] = np.NaN
+        self.x_with_inf = np.copy(self.x).astype(np.float)
+        self.x_with_inf[-1] = np.Inf
+        self.empty = np.array([])
 
     def test_origin(self):
-        """Check beta->0 limit for binary sequence"""
+        """Check beta->0 limit for fixed binary sequence"""
         test_origin(self.x, self.y)
         
+    def test_origin_stochastic(self):
+        """Check beta->0 limit for stochastic binary sequence"""
+        eb = embo.EmpiricalBottleneck(self.xcorr, self.ycorr)
+        test_origin(self.xcorr, self.ycorr)
+        
     def test_asymptote(self):
-        """Check beta->infinity limit for binary sequence"""
+        """Check beta->infinity limit for fixed binary sequence"""
         test_asymptote(self.x, self.y)
+
+    def test_asymptote_stochastic(self):
+        """Check beta->infinity limit for stochastic binary sequence"""
+        test_asymptote(self.xcorr, self.ycorr, maxbeta=15)
+
+    def test_empty_arrays(self):
+        """Check that computing the IB on a pair of empty arrays just returns zero"""
+        eb = embo.EmpiricalBottleneck(self.empty, self.empty)
+        i_x, i_y, _, mixy, hx, hy = eb.get_empirical_bottleneck(return_entropies=True)
+        np.testing.assert_equal(i_x, np.zeros(1))
+        np.testing.assert_equal(i_y, np.zeros(1))
+        np.testing.assert_equal(mixy, np.zeros(1))
+        np.testing.assert_equal(hx, np.zeros(1))
+        np.testing.assert_equal(hy, np.zeros(1))
+
+    def test_array_with_nan(self):
+        """Check that an error is raised in presence of NaNs"""
+        with self.assertRaises(
+                ValueError,
+                msg="The observation data contains NaNs or Infs."):
+            embo.EmpiricalBottleneck(self.x_with_nan, self.y)
+
+    def test_array_with_inf(self):
+        """Check that an error is raised in presence of infinities"""
+        with self.assertRaises(
+                ValueError,
+                msg="The observation data contains NaNs or Infs."):
+            embo.EmpiricalBottleneck(self.x_with_inf, self.y)
 
 
 class TestUpperBound(unittest.TestCase):
@@ -133,11 +184,6 @@ class TestArbitraryAlphabet(unittest.TestCase):
     def test_asymptote(self):
         """Check beta->infinity limit for sequence with arbitrary alphabet"""
         test_asymptote(self.x, self.y)
-
-class TestEmptyInputs(unittest.TestCase):
-    def setUp(self):
-        self.x = np.array([])
-        self.y = np.array([])
 
 
 if __name__ == "__main__":
